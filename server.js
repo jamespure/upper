b836ee45f47aa42c50d82184cfd2bd623f40492e
Seymour James <seymourpjames@gmail.com>
2022-02-14 21:06:41 -0700

    .....

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv");
+// require("dotenv");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
